8f858a5b518da7d69930f1aad7fc791ea3f9b802
Mihail Bălan <mihail.balan@gmail.com>
2016-02-20 23:39:11 +0200

    gslx. stdx

---
+++ include/bolov/gslx/gslx.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <utility>
+
+#include <bolov/stdx/stdx.h>
+
+namespace bolov {
+namespace gslx {
+
+using size_t = std::ptrdiff_t;
+
+constexpr auto size_cast(std::size_t size) -> gslx::size_t
+{
+    return static_cast<gslx::size_t>(size);
+}
+
+template <class T>
+constexpr auto size_of = size_cast(sizeof(T));
+
+template <class T>
+constexpr auto align_of = size_cast(alignof(T));
+
+template <class T, class Enable = stdx::void_t<decltype(std::declval<T>().size())>>
+constexpr auto size(const T& obj) -> gslx::size_t
+{
+    return size_cast(obj.size());
+}
+
+template <class T, std::size_t N>
+constexpr auto size(const T (&)[N]) -> gslx::size_t
+{
+    return size_cast(N);
+}
+}
+}
---
+++ include/bolov/stdx/stdx.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace bolov {
+namespace stdx {
+
+template <class...>
+using void_t = void;
+}
+}
