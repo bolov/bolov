6439b0fa2207a32c5a22b3674c82b90de134e053
Mihail Bălan <mihail.balan@gmail.com>
2016-04-07 16:57:18 +0300

    ::str string_to

--- include/bolov/strings.h
+++ include/bolov/strings.h
@@ -170,5 +170,145 @@ auto trim(std::basic_string<Char_t, Traits, Allocator>& str, F pred = detail::is
 {
     trim(str, get_trimmed(str, pred));
 }
+
+namespace detail {
+template <class Char_t, class Traits, class Allocator>
+auto to_ascii(const std::basic_string<Char_t, Traits, Allocator>& str) -> std::string
+{
+    std::string ascii;
+    ascii.reserve(str.size());
+
+    for (auto ch : str) {
+        if (ch >= 0 && ch <= 127) {
+            ascii.push_back(ch);
+        } else {
+            ascii.push_back('?');
+        }
+    }
+    return ascii;
+}
+
+auto to_ascii(std::string& str) -> std::string & { return str; }
+auto to_ascii(const std::string& str) -> const std::string & { return str; }
+
+template <class Char_t, class Cast_t, class Enable = void>
+struct String_cast_traits {
+};
+
+template <class Cast_t>
+struct String_cast_traits<
+    char, Cast_t, std::enable_if_t<stdx::is_integral_v<Cast_t> && stdx::is_signed_v<Cast_t>>> {
+    static auto strto(const char* str, char** str_end, int base = 0) -> long long
+    {
+        return std::strtoll(str, str_end, base);
+    }
+};
+template <class Cast_t>
+struct String_cast_traits<
+    char, Cast_t, std::enable_if_t<stdx::is_integral_v<Cast_t> && stdx::is_unsigned_v<Cast_t>>> {
+    static auto strto(const char* str, char** str_end, int base = 0) -> unsigned long long
+    {
+        return std::strtoull(str, str_end, base);
+    }
+};
+template <>
+struct String_cast_traits<char, float> {
+    static auto strto(const char* str, char** str_end, int = 0) -> float
+    {
+        return std::strtof(str, str_end);
+    }
+};
+template <>
+struct String_cast_traits<char, double> {
+    static auto strto(const char* str, char** str_end, int = 0) -> double
+    {
+        return std::strtod(str, str_end);
+    }
+};
+template <class Cast_t>
+
+struct String_cast_traits<
+    wchar_t, Cast_t, std::enable_if_t<stdx::is_integral_v<Cast_t> && stdx::is_signed_v<Cast_t>>> {
+    static auto strto(const wchar_t* str, wchar_t** str_end, int base = 0) -> long long
+    {
+        return std::wcstoll(str, str_end, base);
+    }
+};
+template <class Cast_t>
+struct String_cast_traits<
+    wchar_t, Cast_t, std::enable_if_t<stdx::is_integral_v<Cast_t> && stdx::is_unsigned_v<Cast_t>>> {
+    static auto strto(const wchar_t* str, wchar_t** str_end, int base = 0) -> unsigned long long
+    {
+        return std::wcstoull(str, str_end, base);
+    }
+};
+template <>
+struct String_cast_traits<wchar_t, float> {
+    static auto strto(const wchar_t* str, wchar_t** str_end, int = 0) -> float
+    {
+        return std::wcstof(str, str_end);
+    }
+};
+template <>
+struct String_cast_traits<wchar_t, double> {
+    static auto strto(const wchar_t* str, wchar_t** str_end, int = 0) -> double
+    {
+        return std::wcstod(str, str_end);
+    }
+};
+
+} // namespace detail
+
+template <class Cast_t, class Char_t, class Traits, class Allocator>
+auto string_to(const std::basic_string<Char_t, Traits, Allocator>& str)
+    -> stdx::First_of<Cast_t, std::enable_if_t<stdx::is_arithmetic_v<Cast_t>>>
+{
+    using namespace std::string_literals;
+
+    Char_t* last;
+    errno = 0;
+    auto value = detail::String_cast_traits<Char_t, Cast_t>::strto(stdx::begin_ptr(str), &last);
+
+    if (last < stdx::end_ptr(str))
+        throw std::out_of_range{"string '"s + detail::to_ascii(str) + "' does not represent a `"s +
+                                rtti::type_name<Cast_t> + "` value"};
+
+    try {
+        return gsl::narrow<Cast_t>(value);
+    } catch (gsl::narrowing_error&) {
+        throw std::out_of_range{"value in string '"s + detail::to_ascii(str) +
+                                "' is out of range for `"s + rtti::type_name<Cast_t> + "`"};
+    } catch(...) {
+        throw;
+    }
+}
+
+template <class Cast_t, class String_span_t>
+auto string_to(const String_span_t& str)
+    -> stdx::First_of<Cast_t,
+                      std::enable_if_t<stdx::is_arithmetic_v<Cast_t>>,
+                      decltype(gslx::as_const_basic_string_span(str))>
+{
+    return string_to<Cast_t>(gsl::to_string(gslx::as_const_basic_string_span(str)));
+}
+
+namespace detail {
+template <class Tuple_t, class Char_t, class Traits, class Allocator, std::size_t... I>
+auto string_to_tuple(const std::basic_string<Char_t, Traits, Allocator>& str,
+                     std::index_sequence<I...>) -> Tuple_t
+{
+    const auto tokens = get_split(str);
+    return Tuple_t{string_to<stdx::Tuple_get_t<Tuple_t, I>>(tokens[I])...};
 }
 }
+
+template <class... Cast_ts, class Char_t, class Traits, class Allocator>
+auto string_to(const std::basic_string<Char_t, Traits, Allocator>& str)
+    -> stdx::First_of<std::tuple<Cast_ts...>, std::enable_if_t<(sizeof...(Cast_ts) > 1)>>
+{
+    return detail::string_to_tuple<std::tuple<Cast_ts...>>(
+        str, std::make_index_sequence<sizeof...(Cast_ts)>());
+}
+
+} // namespace str
+} // namespace bolov
